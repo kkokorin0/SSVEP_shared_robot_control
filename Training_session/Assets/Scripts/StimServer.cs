b1eea6a04dada26862c21630b6e41f83fd131ed1
Kirill Kokorin <kkokorin@synchron.com>
2024-01-12 16:49:49 +1100

    Align axis to QR code

--- Training_session/Assets/Scripts/StimServer.cs
+++ Training_session/Assets/Scripts/StimServer.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using UnityEngine;
 using System.Threading;
+using Microsoft.MixedReality.OpenXR;
 using Microsoft.MixedReality.SampleQRCodes;
+using Microsoft.MixedReality.QR;
+using Microsoft.MixedReality.Toolkit.Utilities;
 
 public class StimServer : MonoBehaviour
 {   // server
@@ -23,6 +27,10 @@ public class StimServer : MonoBehaviour
 
     // QR code tracking
     public QRCodesManager QrCodesManager;
+    private Microsoft.MixedReality.QR.QRCode _qrCode;
+    private Guid _qrCoords;
+    public Pose QRCodePose;
+    public GameObject QRCodeFrame;
 
     void Start()
     {
@@ -136,12 +144,68 @@ public class StimServer : MonoBehaviour
         }
     }
 
+    //Get pose in Unity coordinates based on spatial graph node id
+    /*private Pose GetPose(Guid spatialGraphNodeId)
+    {
+        System.Numerics.Matrix4x4? relativePose = System.Numerics.Matrix4x4.Identity;
+
+        SpatialGraphNode coordinateSystem = SpatialGraphNode.FromStaticNodeId(spatialGraphNodeId);
+
+        /*SpatialCoordinateSystem coordinateSystem =
+          Windows.Perception.Spatial.Preview.SpatialGraphInteropPreview.
+            CreateCoordinateSystemForNode(spatialGraphNodeId);#1#
+
+        SpatialCoordinateSystem rootSpatialCoordinateSystem =
+          (SpatialCoordinateSystem)System.Runtime.InteropServices.Marshal.
+              GetObjectForIUnknown(WorldManager.GetNativeISpatialCoordinateSystemPtr());
+
+        // Get the relative transform from the unity origin
+        relativePose = coordinateSystem.TryGetTransformTo(rootSpatialCoordinateSystem);
+
+        System.Numerics.Matrix4x4 newMatrix = relativePose.Value;
+
+        // Platform coordinates are all right handed and unity uses left handed matrices. 
+        // so we convert the matrix from rhs-rhs to lhs-lhs 
+        newMatrix.M13 = -newMatrix.M13;
+        newMatrix.M23 = -newMatrix.M23;
+        newMatrix.M43 = -newMatrix.M43;
+
+        newMatrix.M31 = -newMatrix.M31;
+        newMatrix.M32 = -newMatrix.M32;
+        newMatrix.M34 = -newMatrix.M34;
+
+        System.Numerics.Vector3 scale;
+        System.Numerics.Quaternion rotation1;
+        System.Numerics.Vector3 translation1;
+
+        System.Numerics.Matrix4x4.Decompose(newMatrix, out scale, out rotation1,
+                                            out translation1);
+        var translation = new Vector3(translation1.X, translation1.Y, translation1.Z);
+        var rotation = new Quaternion(rotation1.X, rotation1.Y, rotation1.Z, rotation1.W);
+        var pose = new Pose(translation, rotation);
+
+        // If there is a parent to the camera that means we are using teleport and we
+        // should not apply the teleport to these objects so apply the inverse
+        if (CameraCache.Main.transform.parent != null)
+        {
+            pose = pose.GetTransformedBy(CameraCache.Main.transform.parent);
+        }
+
+        return pose;
+    }*/
+
     void Update()
     {
         // look for QR code and setup global coords
-        /*if (QrCodesManager.GetList().Count == 0) return;
-        Debug.Log(QrCodesManager.GetList()[0].ToString());
-        QrCodesManager.StopQRTracking();*/
+        if (QrCodesManager.GetList().Count == 0) return;
+        _qrCode = QrCodesManager.GetList()[0];
+        _qrCoords = _qrCode.SpatialGraphNodeId;
+        SpatialGraphNode coordinateSystem = SpatialGraphNode.FromStaticNodeId(_qrCoords);
+        coordinateSystem.TryLocate(FrameTime.OnUpdate, out Pose QRCodePose);
+        QRCodeFrame.transform.SetPositionAndRotation(QRCodePose.position, QRCodePose.rotation);
+
+        /*Debug.Log(QrCodesManager.GetList()[0].ToString());*/
+        /*QrCodesManager.StopQRTracking();*/
 
     }
 }
